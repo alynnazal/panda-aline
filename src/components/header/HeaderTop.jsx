4c4afb0f4b1fb6f2e28724c25d7cd8b4f5ce19c8
hasan-tfaily <hasan.tf.11@outlook.com>
2025-12-12 13:23:21 +0200

    Refactor HeaderTop component by removing unused list items and cleaning up layout for better readability.

--- src/components/header/HeaderTop.jsx
+++ src/components/header/HeaderTop.jsx
@@ -9,7 +9,6 @@ export const HeaderTop = () => {
         <div className="row">
           <div className="col-xl-6 col-lg-8">
             <ul className="tg-header__top-info left-side list-wrap">
-              
               <li>
                 <i className="flaticon-mail"></i>
                 <a href="kontakt@fahrdienst-panda.de">
@@ -18,32 +17,14 @@ export const HeaderTop = () => {
               </li>
               <li>
                 <i className="flaticon-placeholder"></i> +49 176 63263948
-            
               </li>
             </ul>
           </div>
           <div className="col-xl-6 col-lg-4">
             <ul className="tg-header__top-right list-wrap">
               <li>
-                <i className="flaticon-three-o-clock-clock"></i>Wir sind 24/7 für Sie da
-              </li>
-              <li className="tg-header__top-social">
-                <ul className="list-wrap">
-                  <li>
-                  </li>
-                  <li>
-                   
-                  </li>
-                  <li>
-                    
-                  </li>
-                  <li>
-                    
-                  </li>
-                  <li>
-                    
-                  </li>
-                </ul>
+                <i className="flaticon-three-o-clock-clock"></i>Wir sind 24/7
+                für Sie da
               </li>
             </ul>
           </div>
