d35de61ab0ea6bee92d6625ee32d566106b5a2e7
hasan-tfaily <hasan.tf.11@outlook.com>
2025-12-12 14:53:06 +0200

    Refactor footer layout and styles for improved responsiveness and readability; adjust gutter and padding in CSS, and clean up JSX structure in FooterOne component.

--- src/components/footer/FooterOne.jsx
+++ src/components/footer/FooterOne.jsx
@@ -11,21 +11,42 @@ export const FooterOne = () => {
               <div className="footerNew">
                 <div className="col-xl-2 col-lg-4 col-md-6 col-sm-6">
                   <div className="footer__widget">
-                    <h4 className="footer__widget-title" style={{ textAlign: "center" }}>
+                    <h4
+                      className="footer__widget-title"
+                      style={{ textAlign: "center" }}
+                    >
                       Kontakt
                     </h4>
-                    <div className="footer__link" style={{ display: "flex", justifyContent: "center" }}>
-                      <ul className="list-wrap" style={{ textAlign: "center", padding: 0 }}>
+                    <div
+                      className="footer__link"
+                      style={{ display: "flex", justifyContent: "center" }}
+                    >
+                      <ul
+                        className="list-wrap"
+                        style={{ textAlign: "center", padding: 0 }}
+                      >
                         <li>
                           <a
                             href="mailto:kontakt@fahrdienst-panda.de"
-                            style={{ color: "#fff",whiteSpace: "nowrap", display: "inline-block", width: "100%" }}
+                            style={{
+                              color: "#fff",
+                              whiteSpace: "nowrap",
+                              display: "inline-block",
+                              width: "100%",
+                            }}
                           >
                             kontakt@fahrdienst-panda.de
                           </a>
                         </li>
                         <li>
-                          <a href="tel:+4917663263948" style={{ color: "#fff", display: "inline-block", width: "100%" }}>
+                          <a
+                            href="tel:+4917663263948"
+                            style={{
+                              color: "#fff",
+                              display: "inline-block",
+                              width: "100%",
+                            }}
+                          >
                             +49 176 63263948
                           </a>
                         </li>
@@ -33,28 +54,30 @@ export const FooterOne = () => {
                     </div>
                   </div>
                 </div>
-                <div className="col-xl-3 col-lg-4 col-md-6">
-                  <div className="footer__widget">
-                    <div className="footer__social">
-                      <ul className="list-wrap">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
 
                 <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6">
                   <div className="footer__widget">
-                    <h4 className="footer__widget-title" style={{ textAlign: "center" }}>
+                    <h4
+                      className="footer__widget-title"
+                      style={{ textAlign: "center" }}
+                    >
                       Standort
                     </h4>
-                    <div className="footer__link" style={{ display: "flex", justifyContent: "center" }}>
-                      <ul className="list-wrap" style={{ textAlign: "center", padding: 0 }}>
-                        <li style={{ color: "#fff", display: "inline-block", width: "100%" }}>
+                    <div
+                      className="footer__link"
+                      style={{ display: "flex", justifyContent: "center" }}
+                    >
+                      <ul
+                        className="list-wrap"
+                        style={{ textAlign: "center", padding: 0 }}
+                      >
+                        <li
+                          style={{
+                            color: "#fff",
+                            display: "inline-block",
+                            width: "100%",
+                          }}
+                        >
                           Neunburger Straße 22B
                           <br />
                           10969 Berlin
@@ -71,7 +94,9 @@ export const FooterOne = () => {
             <div className="container">
               <div className="row align-items-center">
                 <div className="copyright-text text-center">
-                  <p style={{ color: "#fff" }}>Copyright © 2025. All Rights Reserved.</p>
+                  <p style={{ color: "#fff" }}>
+                    Copyright © 2025. All Rights Reserved.
+                  </p>
                 </div>
               </div>
             </div>
